bf29fa8646f45c3d2f22e2529c965c9f313cd182
RingoTangs <1466637477@qq.com>
2023-09-08 23:37:41 +0800

    feat(custom): 新增 mapInit 初始化 map

--- src/mapbox.ts
+++ src/mapbox.ts
@@ -5,4 +5,24 @@ const ACCESS_TOKEN =
 
 mapboxgl.accessToken = ACCESS_TOKEN
 
-export { mapboxgl }
+function mapInit(container: HTMLElement | string) {
+    const map = new mapboxgl.Map({
+        container,
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 1, // starting zoom
+    })
+
+    map.on('styledata', () => {
+        map.setFog({
+            color: 'rgb(186, 210, 235)', // Lower atmosphere
+            'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+            'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+            'space-color': 'rgb(11, 11, 25)', // Background color
+            'star-intensity': 0.6, // Background star brightness (default 0.35 at low zoooms )
+        })
+    })
+    return map
+}
+
+export { mapboxgl, mapInit }
