1b8e11c5be06e289bab2ee6ebf91d045d695e54e
RingoTangs <1466637477@qq.com>
2023-09-08 23:38:50 +0800

    feat(custom): 大气层样式

---
+++ src/components/02_AtmosphereStyle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { mapInit } from '@/mapbox'
+import { ref, onMounted } from 'vue'
+
+const mapbox = ref<HTMLDivElement>()
+
+onMounted(() => mapInit(mapbox.value!))
+</script>
+
+<template>
+    <div ref="mapbox" class="map" id="map"></div>
+</template>
+
+<style scoped>
+.map {
+    width: 100%;
+    height: 100%;
+}
+</style>
