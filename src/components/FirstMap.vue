a5f427023dea5a1606b41125c564af5feddbdd81
RingoTangs <1466637477@qq.com>
2023-09-08 20:57:53 +0800

    feat(custom): first Map

---
+++ src/components/FirstMap.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { mapboxgl } from '@/mapbox'
+import { ref, onMounted } from 'vue'
+
+const mapbox = ref<HTMLDivElement>()
+
+onMounted(() => {
+    new mapboxgl.Map({
+        container: mapbox.value!,
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 8, // starting zoom
+    })
+})
+</script>
+
+<template>
+    <div ref="mapbox" class="map"></div>
+</template>
+
+<style scoped>
+.map {
+    width: 100%;
+    height: 100%;
+}
+</style>
