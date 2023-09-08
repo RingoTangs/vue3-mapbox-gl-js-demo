59d00252e0e8d0959d640cb5ef4094d165e3225b
RingoTangs <1466637477@qq.com>
2023-09-08 20:48:30 +0800

    feat(custom): 封装mapbox

---
+++ src/mapbox.ts
@@ -0,0 +1,8 @@
+import mapboxgl from 'mapbox-gl'
+
+const ACCESS_TOKEN =
+    'pk.eyJ1Ijoic2luZ3JhaW55YW5nODA4NiIsImEiOiJjbGxvb3g4emcwYm14M2pwMnZtYTVqM3c2In0.ghcWWHmXTSI5khIitwiA5g'
+
+mapboxgl.accessToken = ACCESS_TOKEN
+
+export { mapboxgl }
