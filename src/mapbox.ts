e88ffde52fffd565fb15b128fd507dcec85510fc
RingoTangs <1466637477@qq.com>
2023-09-09 09:06:20 +0800

    fix(custom): 修改accessToken

--- src/mapbox.ts
+++ src/mapbox.ts
@@ -1,7 +1,7 @@
 import mapboxgl from 'mapbox-gl'
 
 const ACCESS_TOKEN =
-    'pk.eyJ1Ijoic2luZ3JhaW55YW5nODA4NiIsImEiOiJjbGxvb3g4emcwYm14M2pwMnZtYTVqM3c2In0.ghcWWHmXTSI5khIitwiA5g'
+    'pk.eyJ1Ijoic2luZ3JhaW55YW5nODA4NiIsImEiOiJjbGxvcG9xNHgwYmRjM3Fwb2Q3eHF3cmJvIn0.W_onlq_G0GGN4Wcf_SZCzg'
 
 mapboxgl.accessToken = ACCESS_TOKEN
 
