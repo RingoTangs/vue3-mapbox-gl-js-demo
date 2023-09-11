ae03b20b6e3a85382d3fbbbac479eedb910657a8
RingoTangs <1466637477@qq.com>
2023-09-11 22:12:01 +0800

    feat(custom): app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+// import FirstMap from '@/components/01_FirstMap.vue'
+// import AtmosphereStyle from '@/components/02_AtmosphereStyle.vue'
+// import DrawPloygon from '@/components/03_DrawPloygon.vue'
+// import MapOptions from "@/components/04_MapOptions.vue"
+// import InteractionHandlers from '@/components/05_InteractionHandlers.vue'
+// import AddController from '@/components/06_AddController.vue'
+// import UseMapboxTest from '@/components/07_UseMapboxTest.vue'
+// import Add3DTerrain from '@/components/08_Add3DTerrain.vue'
+// import AddCustomIconsWithMarkers from '@/components/09_AddCustomIconsWithMarkers/Index.vue'
+// import CreateDraggableMarker from '@/components/10_CreateDraggableMarker/Index.vue'
+// import DisplayPopup from '@/components/11_DisplayPopup/Index.vue'
+// import DisplayPopupOnHover from '@/components/12_DisplayPopupOnHover/Index.vue'
+// import MyControl from '@/components/13_MyControl/Index.vue'
+// import AttachPopupToMarker from '@/components/14_AttachPopupToMarker/Index.vue'
+import ScaleControl from '@/components/15_ScaleControl/Index.vue'
 </script>
 
 <template>
-    <HelloWorld />
+    <!-- <HelloWorld /> -->
+    <!-- <FirstMap /> -->
+    <!-- <AtmosphereStyle /> -->
+    <!-- <DrawPloygon /> -->
+    <!-- <MapOptions /> -->
+    <!-- <InteractionHandlers /> -->
+    <!-- <AddController /> -->
+    <!-- <UseMapboxTest /> -->
+    <!-- <Add3DTerrain /> -->
+    <!-- <AddCustomIconsWithMarkers/> -->
+    <!-- <CreateDraggableMarker /> -->
+    <!-- <DisplayPopup /> -->
+    <!-- <DisplayPopupOnHover /> -->
+    <!-- <MyControl /> -->
+    <!-- <AttachPopupToMarker /> -->
+    <ScaleControl />
 </template>
