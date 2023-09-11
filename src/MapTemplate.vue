e471e61faadbe0eefed009e9648dabe91e5e6a16
RingoTangs <1466637477@qq.com>
2023-09-11 19:51:37 +0800

    feat(custom): map 初始化模板

---
+++ src/MapTemplate.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useMapbox } from '@/mapbox'
+import { ref, onMounted } from 'vue'
+
+defineOptions({ name: 'MyControl' })
+
+const { map, container } = useMapbox(true)
+const mapboxContainerWrapperRef = ref<HTMLDivElement>()
+
+// Map mounted
+onMounted(() => {
+    const firstChild = mapboxContainerWrapperRef.value.firstChild
+    mapboxContainerWrapperRef.value?.insertBefore(container, firstChild)
+    map.resize()
+})
+</script>
+
+<template>
+    <div class="mapbox-container-wrapper" ref="mapboxContainerWrapperRef"></div>
+</template>
+
+<style scoped>
+.mapbox-container-wrapper {
+    width: 100%;
+    height: 100%;
+}
+</style>
