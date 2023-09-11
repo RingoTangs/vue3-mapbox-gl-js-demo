1c42b1474e5160a64aff4e0d709a4a2c2e076a7e
RingoTangs <1466637477@qq.com>
2023-09-11 22:36:34 +0800

    fix(custom): 添加问题解决链接注释

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,3 +1,7 @@
+/**
+ * 链接：
+ * https://stackoverflow.com/questions/70895690/ts2307-cannot-find-module-app-vue-or-its-corresponding-type-declarations
+ */
 /* eslint-disable */
 declare module '*.vue' {
     import type { DefineComponent } from 'vue'
