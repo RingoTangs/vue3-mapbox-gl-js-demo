1e6c3b6000a31fbf265a0b0c6725250f80fbdc62
RingoTangs <1466637477@qq.com>
2023-09-14 11:23:32 +0800

    feat(custom): 测试 mapbox zoom control 等响应式

--- src/components/03_CreateMapbox.vue
+++ src/components/03_CreateMapbox.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import MaxBox from '@/components/mapbox/Mapbox.vue'
+import { ref } from 'vue'
+import {
+    NavigationControlOptions,
+    FullScreenControlOptions,
+} from '@/components/mapbox/mapbox.ts'
+
+const navCtr = ref<NavigationControlOptions>(true)
+
+const fsCtr = ref<FullScreenControlOptions>(true)
 </script>
 
 <template>
-    <MaxBox init-fog :zoom="5" />
+    <MaxBox init-fog :nav-ctr="navCtr" :full-screen-ctr="fsCtr" />
 </template>
