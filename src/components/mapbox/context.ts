90517aa22cf23ac48bebaa2d885afc93ae05480b
RingoTangs <1466637477@qq.com>
2023-09-16 10:46:24 +0800

    feat(custom): 新增 Map Context Holder

--- src/components/mapbox/context.ts
+++ src/components/mapbox/context.ts
@@ -7,4 +7,22 @@ const provideMap = (map: mapboxgl.Map) => provide(INJECTION_KEY_MAP, map)
 
 const useMap = () => inject(INJECTION_KEY_MAP)
 
-export { INJECTION_KEY_MAP, provideMap, useMap }
+const mapboxContextHolder = () => {
+    const holder: { [id: string]: mapboxgl.Map } = {}
+    const setMap = (id: string, map: mapboxgl.Map) => {
+        if (holder[id]) {
+            throw new Error('mapbox context holder has exists id: ' + id)
+        }
+        holder[id] = map
+    }
+    const getMap = (id: string): mapboxgl.Map | undefined => holder[id]
+
+    return {
+        setMap,
+        getMap,
+    }
+}
+
+const MAPBOX_CONTEXT_HOLDER = mapboxContextHolder()
+
+export { INJECTION_KEY_MAP, MAPBOX_CONTEXT_HOLDER, provideMap, useMap }
