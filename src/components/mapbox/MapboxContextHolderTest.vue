604205f2354990c7112d966cbbb9e4785108620a
RingoTangs <1466637477@qq.com>
2023-09-16 13:16:13 +0800

    feat(custom): 测试 MapboxContextHolder

---
+++ src/components/mapbox/MapboxContextHolderTest.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { MAPBOX_CONTEXT_HOLDER } from './context.ts'
+
+console.log(MAPBOX_CONTEXT_HOLDER.getMap('HelloMapbox'))
+</script>
+
+<template>
+    <template v-if="false"></template>
+</template>
